a33bd1c39ce382b480894a98068bfafe49c3ba3d
delmar almeida <angelusdelmar@gmail.com>
2026-07-17 22:39:54 +0100

    Chore: remove TableSelector from main index.ts

--- src/features/tables/index.ts
+++ src/features/tables/index.ts
@@ -3,7 +3,6 @@ import TableShow from "./pages/TableShow/TableShow";
 import TableCreate from "./pages/TableCreate/TableCreate";
 import TableEdit from "./pages/TableEdit/TableEdit";
 import TablesRoller from "./pages/TablesRoller/TablesRoller";
-import TableSelector from "./components/TableSelector/TableSelector";
 import findMatchingRow from "./logic/findMatchingRow";
 import tableStorage from "./logic/tableStorage";
 import buildTable from "./logic/buildTable";
@@ -15,7 +14,6 @@ export {
   TableCreate,
   TableEdit,
   TablesRoller,
-  TableSelector,
   findMatchingRow,
   tableStorage,
   buildTable,
